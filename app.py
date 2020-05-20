170db09b3d04269225d0d7a9178448304ea754e7
dinhbaouit <dinhbaouit@gmail.com>
2020-05-20 18:02:51 +0700

    fix backtrace

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ import getopt
 
 def printusage():
 	print """
-	Usage: 
-	Trace class   : python app.py [option] -c -p [process] class1 class2 class3 ...
-	Trace function: python app.py [option] -f -p [process] function1, function2, function3 ...\n
+Usage: 
+Trace class   : python app.py [option] -c -p [process] class1 class2 class3 ...
+Trace function: python app.py [option] -f -p [process] function1, function2, function3 ...\n
 
-	Option:
-	-n,    --no-backtrace      Set no backtrace
-	"""
+Option:
+-n      Set no backtrace
+"""
 	sys.exit(2)
 
 
@@ -25,10 +25,10 @@ def jscode_traceclass(class_list,backtrace_flag):
 		bt = ""
 	return """
 var Color = {
-    RESET: "\\x1b[39;49;00m", Black: "0;01", Blue: "4;01", Cyan: "6;01", Gray: "7;11", Green: "2;01", Purple: "5;01", Red: "1;01", Yellow: "3;01",
-    Light: {
-        Black: "0;11", Blue: "4;11", Cyan: "6;11", Gray: "7;01", Green: "2;11", Purple: "5;11", Red: "1;11", Yellow: "3;11"
-    }
+	RESET: "\\x1b[39;49;00m", Black: "0;01", Blue: "4;01", Cyan: "6;01", Gray: "7;11", Green: "2;01", Purple: "5;01", Red: "1;01", Yellow: "3;01",
+	Light: {
+		Black: "0;11", Blue: "4;11", Cyan: "6;11", Gray: "7;01", Green: "2;11", Purple: "5;11", Red: "1;11", Yellow: "3;11"
+	}
 };
 
 /**
@@ -42,112 +42,112 @@ var Color = {
  * }
  */
 var LOG = function (input, kwargs) {
-    kwargs = kwargs || {};
-    var logLevel = kwargs['l'] || 'log', colorPrefix = '\\x1b[3', colorSuffix = 'm';
-    if (typeof input === 'object')
-        input = JSON.stringify(input, null, kwargs['i'] ? 2 : null);
-    if (kwargs['c'])
-        input = colorPrefix + kwargs['c'] + colorSuffix + input + Color.RESET;
-    console[logLevel](input);
+	kwargs = kwargs || {};
+	var logLevel = kwargs['l'] || 'log', colorPrefix = '\\x1b[3', colorSuffix = 'm';
+	if (typeof input === 'object')
+		input = JSON.stringify(input, null, kwargs['i'] ? 2 : null);
+	if (kwargs['c'])
+		input = colorPrefix + kwargs['c'] + colorSuffix + input + Color.RESET;
+	console[logLevel](input);
 
 };
 
 var printBacktrace = function () {
-    Java.perform(function() {
-        var android_util_Log = Java.use('android.util.Log'), java_lang_Exception = Java.use('java.lang.Exception');
-        // getting stacktrace by throwing an exception
-        LOG(android_util_Log.getStackTraceString(java_lang_Exception.$new()), { c: Color.Gray });
-    });
+	Java.perform(function() {
+		var android_util_Log = Java.use('android.util.Log'), java_lang_Exception = Java.use('java.lang.Exception');
+		// getting stacktrace by throwing an exception
+		LOG(android_util_Log.getStackTraceString(java_lang_Exception.$new()), { c: Color.Gray });
+	});
 };
 
 function traceClass(targetClass) {
-    var hook;
-    try {
-        hook = Java.use(targetClass);
-    } catch (e) {
-        console.error("trace class failed", e);
-        return;
-    }
+	var hook;
+	try {
+		hook = Java.use(targetClass);
+	} catch (e) {
+		console.error("trace class failed", e);
+		return;
+	}
 
-    var methods = hook.class.getDeclaredMethods();
-    hook.$dispose();
+	var methods = hook.class.getDeclaredMethods();
+	hook.$dispose();
 
-    var parsedMethods = [];
-    methods.forEach(function (method) {
-        var methodStr = method.toString();
-        var methodReplace = methodStr.replace(targetClass + ".", "TOKEN").match(/\\sTOKEN(.*)\\(/)[1];
-         parsedMethods.push(methodReplace);
-    });
+	var parsedMethods = [];
+	methods.forEach(function (method) {
+		var methodStr = method.toString();
+		var methodReplace = methodStr.replace(targetClass + ".", "TOKEN").match(/\\sTOKEN(.*)\\(/)[1];
+		 parsedMethods.push(methodReplace);
+	});
 
-    uniqBy(parsedMethods, JSON.stringify).forEach(function (targetMethod) {
-        traceMethod(targetClass + '.' + targetMethod);
-    });
+	uniqBy(parsedMethods, JSON.stringify).forEach(function (targetMethod) {
+		traceMethod(targetClass + '.' + targetMethod);
+	});
 }
 
 function traceMethod(targetClassMethod) {
-    var delim = targetClassMethod.lastIndexOf('.');
-    if (delim === -1)
-        return;
+	var delim = targetClassMethod.lastIndexOf('.');
+	if (delim === -1)
+		return;
 
-    var targetClass = targetClassMethod.slice(0, delim);
-    var targetMethod = targetClassMethod.slice(delim + 1, targetClassMethod.length);
+	var targetClass = targetClassMethod.slice(0, delim);
+	var targetMethod = targetClassMethod.slice(delim + 1, targetClassMethod.length);
 
-    var hook = Java.use(targetClass);
-    var overloadCount = hook[targetMethod].overloads.length;
+	var hook = Java.use(targetClass);
+	var overloadCount = hook[targetMethod].overloads.length;
 
-    LOG({ tracing: targetClassMethod, overloaded: overloadCount }, { c: Color.Blue });
+	LOG({ tracing: targetClassMethod, overloaded: overloadCount }, { c: Color.Blue });
 
-    for (var i = 0; i < overloadCount; i++) {
-        hook[targetMethod].overloads[i].implementation = function () {
-            var log = { '#': targetClassMethod, args: [] };
+	for (var i = 0; i < overloadCount; i++) {
+		hook[targetMethod].overloads[i].implementation = function () {
+			var log = { '#': targetClassMethod, args: [] };
 
-            for (var j = 0; j < arguments.length; j++) {
-                var arg = arguments[j];
-                // quick&dirty fix for java.io.StringWriter char[].toString() impl because frida prints [object Object]
-                if (j === 0 && arguments[j]) {
-                    if (arguments[j].toString() === '[object Object]') {
-                        var s = [];
-                        for (var k = 0, l = arguments[j].length; k < l; k++) {
-                            s.push(arguments[j][k]);
-                        }
-                        arg = s.join('');
-                    }
-                }
-                log.args.push({ i: j, o: arg, s: arg ? arg.toString(): 'null'});
-            }
+			for (var j = 0; j < arguments.length; j++) {
+				var arg = arguments[j];
+				// quick&dirty fix for java.io.StringWriter char[].toString() impl because frida prints [object Object]
+				if (j === 0 && arguments[j]) {
+					if (arguments[j].toString() === '[object Object]') {
+						var s = [];
+						for (var k = 0, l = arguments[j].length; k < l; k++) {
+							s.push(arguments[j][k]);
+						}
+						arg = s.join('');
+					}
+				}
+				log.args.push({ i: j, o: arg, s: arg ? arg.toString(): 'null'});
+			}
 
-            var retval;
-            try {
-                retval = this[targetMethod].apply(this, arguments); // might crash (Frida bug?)
-                log.returns = { val: retval, str: retval ? retval.toString() : null };
-            } catch (e) {
-                console.error(e);
-            }
-            console.log("-------------------------------------------");
-            """+bt+""";
-            LOG(log, { c: Color.Green });
+			var retval;
+			try {
+				retval = this[targetMethod].apply(this, arguments); // might crash (Frida bug?)
+				log.returns = { val: retval, str: retval ? retval.toString() : null };
+			} catch (e) {
+				console.error(e);
+			}
+			console.log("-------------------------------------------");
+			"""+bt+""";
+			LOG(log, { c: Color.Green });
 
-            return retval;
-        }
-    }
+			return retval;
+		}
+	}
 }
 
 // remove duplicates from array
 function uniqBy(array, key) {
-    var seen = {};
-    return array.filter(function (item) {
-        var k = key(item);
-        return seen.hasOwnProperty(k) ? false : (seen[k] = true);
-    });
+	var seen = {};
+	return array.filter(function (item) {
+		var k = key(item);
+		return seen.hasOwnProperty(k) ? false : (seen[k] = true);
+	});
 }
 
 
 var Main = function() {
-    Java.perform(function () { // avoid java.lang.ClassNotFoundException
-        [
-            """+class_list_str+"""
-        ].forEach(traceClass);
-    });
+	Java.perform(function () { // avoid java.lang.ClassNotFoundException
+		[
+			"""+class_list_str+"""
+		].forEach(traceClass);
+	});
 };
 
 Java.perform(Main);
@@ -170,42 +170,42 @@ def jscode_tracefunction(function_list, backtrace_flag):
   var overloads = clazz[func].overloads;
   for (var i in overloads) {
 	  
-    if (overloads[i].hasOwnProperty('argumentTypes')) {
-      var parameters = [];
+	if (overloads[i].hasOwnProperty('argumentTypes')) {
+	  var parameters = [];
 
-      var curArgumentTypes = overloads[i].argumentTypes, args = [], argLog = '[';
+	  var curArgumentTypes = overloads[i].argumentTypes, args = [], argLog = '[';
 
-      for (var j in curArgumentTypes) {
-        var cName = curArgumentTypes[j].className;
-        parameters.push(cName);
-        argLog += "'(" + cName + ") ' + v" + j + ",";
-        args.push('v' + j);
-      }
-      argLog += ']';
+	  for (var j in curArgumentTypes) {
+		var cName = curArgumentTypes[j].className;
+		parameters.push(cName);
+		argLog += "'(" + cName + ") ' + v" + j + ",";
+		args.push('v' + j);
+	  }
+	  argLog += ']';
 
-      var script = "var ret = this." + func + '(' + args.join(',') + ") || '';\\n"
+	  var script = "var ret = this." + func + '(' + args.join(',') + ") || '';\\n"
 		+ "console.log('--------------------------------------------------------------------------------');"
-        + "console.log(JSON.stringify(" + argLog + "));\\n"
-        + '    """+bt+"""'
-        + "return ret;"
+		+ "console.log(JSON.stringify(" + argLog + "));\\n"
+		+ '    """+bt+"""'
+		+ "return ret;"
 
-      args.push(script);
-      clazz[func].overload.apply(this, parameters).implementation = Function.apply(null, args);
-    }
+	  args.push(script);
+	  clazz[func].overload.apply(this, parameters).implementation = Function.apply(null, args);
+	}
   }
 }
 
 Java.perform(function() {
-    console.log("-----------------------------------------------");
+	console.log("-----------------------------------------------");
   """+hookoverload+"""
 })
 """
 
 def on_message(message, data):
-    print "============================================"
-    if message["type"] == "send":
-        data = message["payload"]
-        print data
+	print "============================================"
+	if message["type"] == "send":
+		data = message["payload"]
+		print data
 
 def main():
 	process_name = ""
@@ -233,8 +233,9 @@ def main():
 
 		if o == "-p" or "--process":
 			process_name = a
-		if o == "-n" or "--no-backtrace":
+		if o == "-n":
 			backtrace_flag = 0
+
 
 	if option == 1 or option == 0 and args != []:
 		class_list = args
